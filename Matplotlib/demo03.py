ac6d736b5416276f9345ee9087f34c39fe94f62d
lml113 <3285275237@qq.com>
2022-03-03 23:37:08 +0800

    legend 图例

--- Matplotlib/demo03.py
+++ Matplotlib/demo03.py
@@ -8,8 +8,8 @@ import numpy as np
 x = np.linspace(-1,1,50)
 y2 = x**2
 y1 = 2*x+1
-plt.plot(x,y2,label="2x+1")
-plt.plot(x,y1,color = 'red',linewidth = 1.0,linestyle = '--',label = 'x^2')
+l1,=plt.plot(x,y2,label="2x+1")
+l2,=plt.plot(x,y1,color = 'red',linewidth = 1.0,linestyle = '--',label = 'x^2')
 
 #设置边界
 plt.xlim(-1,1)
@@ -24,7 +24,8 @@ ax.spines['bottom'].set_position(('data',0))
 ax.spines['left'].set_position(('data',0))
 
 #图标
-plt.legend(loc='upper left')
+# plt.legend(loc='upper left')
+plt.legend(handles=[l1,l2], labels=['aaa','bbb'], loc='best')
 
 
 plt.show()
