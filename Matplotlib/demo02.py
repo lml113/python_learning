9878635735fe8bce7efaccfad6d2108bf9c1c4f0
lml113 <3285275237@qq.com>
2022-03-03 13:09:08 +0800

    设置坐标轴1

--- Matplotlib/demo02.py
+++ Matplotlib/demo02.py
@@ -14,5 +14,10 @@ plt.xlabel("I am x")
 plt.ylim(-1,1)
 plt.ylabel('I am y')
 
+new_ticks = np.linspace(-2*np.pi,2*np.pi,9)
+print(new_ticks)
+plt.xticks(new_ticks,['-2π', '-1.5π', '-π', '-0.5π', '0','0.5π','π','1.5π','2π'])
+plt.yticks([-1,-0.5,0,0.5,1],[r'$really\ bad$', r'$bad\ \alpha$', r'$normal$', r'$good$', r'$very\ good$'])
+
 plt.title('test01')
 plt.show()
