60d11aa9ec5a6d893ad13f52c91c45bb60b3a46a
lml113 <3285275237@qq.com>
2022-03-06 16:23:29 +0800

    font_color_data

---
+++ CursiveSignature/FCid.py
@@ -0,0 +1,20 @@
+font2ids_dict = {
+                    '一笔艺术签': ['901', '15'],
+                    '连笔商务签': ['904', '15'],
+                    '一笔商务签': ['905', '14'],
+                    '真人手写': ['343', '14'],
+                    '卡通趣圆字': ['397', '14'],
+                    '暴躁字': ['380', '14']
+                    }
+color2ids_dict = {
+                    'Black': ['#000000', '#FFFFFF'],
+                    'Blue': ['#0000FF', '#FFFFFF'],
+                    'Red': ['#FF0000', '#FFFFFF'],
+                    'Green': ['#00FF00', '#FFFFFF'],
+                    'Yellow': ['#FFFF00', '#FFFFFF'],
+                    'Pink': ['#FFC0CB', '#FFFFFF'],
+                    'DeepSkyBlue': ['#00BFFF', '#FFFFFF'],
+                    'Cyan': ['#00FFFF', '#FFFFFF'],
+                    'Orange': ['#FFA500', '#FFFFFF'],
+                    'Seashell': ['#FFF5EE', '#FFFFFF']
+                }
